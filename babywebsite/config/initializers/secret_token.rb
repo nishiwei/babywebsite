55a502d2001c8ec9231a77978b7169d64ae5e74b
eric <nishiwei4209@gmail.com>
2013-07-21 11:43:33 -0700

    initial push

---
+++ babywebsite/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babywebsite::Application.config.secret_key_base = '9b2b1c394955f8468e737155f34305554436dc653c2a8c2dacf6a4ae4b64d848b5ff7bb70dda67a42538d2c4642fbdc7f92bc8385883c5c6e1d4d410622ec3d4'
