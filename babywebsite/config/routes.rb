a9bb7bf6e43f98b7c83b813428541181d41de091
eric <nishiwei4209@gmail.com>
2013-12-23 20:59:07 -0800

    initial design

--- babywebsite/config/routes.rb
+++ babywebsite/config/routes.rb
@@ -7,7 +7,8 @@ Babywebsite::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get '/' => 'index#index'
+	
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
