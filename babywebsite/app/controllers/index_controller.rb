d5ddbde56a6a58bcde25446ad2f23dd8c5185e01
eric <nishiwei4209@gmail.com>
2013-12-25 12:22:43 -0800

    style done

--- babywebsite/app/controllers/index_controller.rb
+++ babywebsite/app/controllers/index_controller.rb
@@ -1,5 +1,17 @@
 class IndexController < ApplicationController
 	def index
+		@tweets = Tweet.all
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	def delete
 
 	end
 end
---
+++ babywebsite/app/models/tweet.rb
@@ -0,0 +1,3 @@
+class Tweet < ActiveRecord::Base
+	# self.table_name = :tweets
+end
